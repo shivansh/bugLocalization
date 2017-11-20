22896ae0495ac33daeb0ead5e8101aabcda87b03
Shivansh Rai <shivansh@freebsd.org>
2017-11-20 06:23:07 +0530

    Add script for inserting a random bug in client code
    The client code needs to be re-compiled after this script is run.

---
+++ instrumentation/insert_bug.sh
@@ -0,0 +1,16 @@
+#!/bin/bash
+# Inserts a bug randomly in the client code.
+
+set -e
+
+filename="testcases/CS425/include/standard.h"
+bug_identifier=$(tail -1 $filename)
+bug_count=5
+
+# Remove any previously inserted directives
+if [[ $bug_identifier == *"#define BUG"* ]]; then
+    sed -i '$ d' $filename
+fi
+
+random_bug_id=$(( ( RANDOM % $bug_count )  + 1 ))
+echo "#define BUG$random_bug_id" >> "$filename"
