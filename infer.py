fc27990a93a77aa416cdeb10cca6ec7027ccefa7
Shivansh Rai <shivansh@freebsd.org>
2017-11-20 08:46:48 +0530

    Pull in fixes from @jaivardhankapoor

--- infer.py
+++ infer.py
@@ -2,19 +2,24 @@ import re
 import numpy as np
 import parse_output
 
-N_ITER = 1000
-eps = 1e-6
+N_ITER = 20
+eps = 1e-3
 
 A = parse_output.get_A()#read data
 # The parts of data will be truth probabilities
 
 npred = A.shape[0]
+print("Number of predicates:", npred)
 nruns = A.shape[1]
+print("Number of runs:", nruns)
 
 # run_status = np.ones(shape=[nruns]) # 0 means fail, 1 means true
 run_status = parse_output.get_run_status() # 0 means fail, 1 means true
 ind_zero = np.where(run_status==0)[0] # Indices for failed runs
+print("Number of failed runs:", ind_zero.size)
 ind_nonzero = np.where(run_status!=0)[0] # Indices for successful runs
+print("Number of failed runs:", ind_nonzero.size)
+
 
 Q = np.ones(shape=[npred])
 Q_compl = np.ones(shape=[npred])
@@ -38,7 +43,11 @@ for n_iter in range(N_ITER):
         R_compl_kj = np.sum(R_compl[:, ind_zero], axis=0)
         R_compl_ij = R_compl[i,ind_zero]
         F[i] = np.sum(A[i,ind_zero]*(R_ij/R_kj))
-        F_compl[i] = np.sum((1-A[i,ind_zero])*(R_compl_ij/R_compl_kj))
+        # if i == 0 and n_iter == 0:
+        #     print(R_kj)
+        #     print(R_ij)
+        #     print(F[i])
+        F_compl[i] = np.sum((1-A[i,ind_zero])*(R_compl_ij/(R_compl_kj+eps)))
         
         R_kj = np.sum(R[:, ind_nonzero], axis=0)
         R_ij = R[i,ind_nonzero]
@@ -46,11 +55,15 @@ for n_iter in range(N_ITER):
         R_compl_ij = R_compl[i,ind_nonzero]
         S[i] = np.sum(A[i,ind_nonzero]*(R_ij/(R_kj+eps)))
         S_compl[i] = np.sum((1-A[i,ind_nonzero])*(R_compl_ij/(R_compl_kj+eps)))
-    R = A*np.tile(run_status,(npred,1))*np.transpose(np.tile(Q,(nruns,1))) + A*np.tile((1-run_status),(npred,1))/np.transpose(np.tile(Q_compl,(nruns,1)))
-    R_compl = (1-A)*np.tile(run_status,(npred,1))*np.transpose(np.tile(Q_compl,(nruns,1))) + (1-A)*np.tile((1-run_status),(npred,1))/np.transpose(np.tile(Q,(nruns,1)))
-    # R_compl = (1-A)*np.transpose(np.tile(run_status,(npred,1)))*np.tile(Q_compl,(1,nruns)) + (1-A)*np.transpose(np.tile((1-run_status),(npred,1)))/np.tile(Q,(1,nruns))
 
+    R = A*np.tile((1-run_status),(npred,1))*np.transpose(np.tile(Q,(nruns,1))) + A*np.tile((run_status),(npred,1))/np.transpose(np.tile(Q,(nruns,1)) + eps)
+    R_compl = (1-A)*np.tile((1-run_status),(npred,1))*np.transpose(np.tile(Q_compl,(nruns,1))) + (1-A)*np.tile((run_status),(npred,1))/np.transpose(np.tile(Q_compl,(nruns,1)) + eps)
+
+    # if n_iter==N_ITER-1 or n_iter==N_ITER-2:
+    #     print(Q)
+    #     print(list(reversed([x for _,x in sorted(zip(Q,range(npred)))])))
 
 
 ranking = list(reversed([x for _,x in sorted(zip(Q,range(npred)))]))
+print("Order of the predicates according to quality:")
 print([s+1 for s in ranking])
--- parse_output.py
+++ parse_output.py
@@ -3,18 +3,25 @@ import numpy as np
 
 def get_A():
 
-    eps = 1e-2
+    eps = 1e-4
 
     with open("./output_data/output.txt",'r') as f:
         nruns = 0
         npred = 0
+        successful_runs = 0
+        failing_case_separate = 0
         for line in f:
             if "Socket instantiated" in line:
                 nruns += 1
+            if "File transfer complete" in line:
+                successful_runs += 1
+            if "Error while connecting socket: Child process terminated with an error!" in line:
+                failing_case_separate += 1
             if line[0].isdigit():
                 npred += 1
-        npred = npred//nruns
+        npred = npred//(2*nruns - failing_case_separate)
 
+    print(npred,nruns)
     with open("./output_data/output.txt",'r') as f:
         true = np.zeros([npred,nruns]) + eps
         false = np.zeros([npred,nruns]) + eps
@@ -38,21 +45,24 @@ def get_A():
 def get_run_status():
     with open("./output_data/output.txt",'r') as f:
         nruns = 0
-        npred = 0
         for line in f:
             if "Socket instantiated" in line:
                 nruns += 1
-            if line[0].isdigit():
-                npred += 1
-        npred = npred//nruns
 
     with open("./output_data/output.txt",'r') as f:
         run_status = np.zeros([nruns])
         run_iter = 0
+        successful_runs = 0
         for line in f:
             if "Socket instantiated" in line:
                 run_iter += 1
             if "File transfer complete" in line:
                 run_status[run_iter - 1] = 1
+                successful_runs += 1
 
+    # print(run_status)
     return run_status
+
+
+# get_A()
+# get_run_status()
