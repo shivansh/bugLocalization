aec56865fae7d8aad62cfa5e5571d258b5c5bad1
Shivansh Rai <shivansh@freebsd.org>
2017-11-20 07:17:36 +0530

    Update driver script

--- instrumentation/main.sh
+++ instrumentation/main.sh
@@ -8,14 +8,19 @@ serve_dir="server/serve_dir"
 # Insert a random bug in client code.
 ./insert_bug.sh
 
-cd testcases/CS425
-make
+(
+    cd testcases/CS425
+    make
 
-# Remove stale predicate values (if existent) from previous run.
-rm -f predicate_values
-touch predicate_values
+    # Remove stale predicate values (if existent) from previous run.
+    rm -f predicate_values
+    touch predicate_values
 
-for file in $serve_dir/*; do
-    ./client_exe shivansh:rai@127.0.0.1 6061 $file &>> predicate_values
-    sleep .5
-done
+    for file in $serve_dir/*; do
+        ./client_exe shivansh:rai@127.0.0.1 6061 $(basename $file) &>> predicate_values
+        sleep .5
+    done
+)
+
+# Collect the generated values of predicates in different runs appropriately.
+mv testcases/CS425/predicate_values ../output_data/output.txt
